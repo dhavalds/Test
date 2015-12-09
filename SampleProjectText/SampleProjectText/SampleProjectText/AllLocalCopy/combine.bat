9bf64bcf93adafd4aae9fb7495f2b0ebbb8c4c68
dhavalds <dhaval.solanki@marutitech.com>
2015-12-09 20:27:19 +0530

    added new folder and changes

--- SampleProjectText/SampleProjectText/SampleProjectText/AllLocalCopy/combine.bat
+++ SampleProjectText/SampleProjectText/SampleProjectText/AllLocalCopy/combine.bat
@@ -1,5 +1,5 @@
 copy /B ^
-publisher.po^
-+common.po^
- ..\locale\en\messages.po
+Publisher\en\publisher.po^
++Common\en\common.po^
+ ..\..\..\..\locale\en\messages.po
 
---
+++ SampleProjectText/SampleProjectText/SampleProjectText/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleProjectText.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Home()
+        {
+            DefaultMessage response = new DefaultMessage();
+            try
+            {
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo.FileName = AppDomain.CurrentDomain.BaseDirectory + "AllLocalCopy\\combine.bat";
+                proc.StartInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory + "AllLocalCopy";
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.UseShellExecute = false;
+
+                proc.Start();
+
+                response.Success = true;
+            }
+            catch (Exception exception)
+            {
+                //Log.Error(exception.Message, exception);
+            }
+            return View(response);
+        }
+
+        public class DefaultMessage
+        {
+            public DefaultMessage()
+            {
+                Success = false;
+                ErrorMessage = string.Empty;
+            }
+
+            public bool Success { get; set; }
+
+            public string ErrorMessage { get; set; }
+        }
+    }
+}
