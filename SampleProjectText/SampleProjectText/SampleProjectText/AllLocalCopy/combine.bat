10cb85dc3fa919bb91809a61425b4ce9c180e581
dhavalds <dhaval.solanki@marutitech.com>
2016-02-12 18:19:22 +0530

    test

--- SampleProjectText/SampleProjectText/SampleProjectText/AllLocalCopy/combine.bat
+++ SampleProjectText/SampleProjectText/SampleProjectText/AllLocalCopy/combine.bat
@@ -5,4 +5,18 @@ Common\en\common.po^
 +space.txt^
 +Publisher\en\publisher.po^
  ..\..\..\..\..\LocalizationSample\locale\en\messages.po
+ 
+ 
+copy /B ^
+Common\es\common.po^
++space.txt^
++Publisher\es\publisher.po^
+ ..\..\..\..\..\LocalizationSample\locale\es\messages.po
+ 
+ 
+copy /B ^
+Common\fr\common.po^
++space.txt^
++Publisher\fr\publisher.po^
+ ..\..\..\..\..\LocalizationSample\locale\fr\messages.po
 
